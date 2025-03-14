06b7d93ee011c85c20b7e9934883a67aace1abb0
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2025-03-14 12:16:23 +0100

    updated to tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   experimental: {
     svg: true
   }
