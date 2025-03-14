a41951f24fcf9fd9591b7212490f3d80056abfd3
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2025-03-14 16:58:28 +0100

    added mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,20 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   experimental: {
     svg: true,
     contentIntellisense: true,
-  }
+  },
+
+  integrations: [mdx()]
 });
